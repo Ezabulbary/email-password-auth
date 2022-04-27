532092099756eb3ad920bae7fdae6e8f45381839
Ezabulbary <ezabul.bary@gmail.com>
2022-04-28 00:05:07 +0600

    handle submit data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { useState } from 'react';
 
 
 const auth = getAuth(app);
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailBlur = event => {
+    setEmail(event.target.value)
+  }
+
+  const handlePasswordBlur = event => {
+    setPassword(event.target.value)
+  }
+
+  const handleFormSubmit =event => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(Result => {
+      const user = Result.user
+      console.log(user)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+    event.preventDefault();
+  }
   return (
-    <div className="App">
-      
-    </div>
+    <form onSubmit={handleFormSubmit}>
+      <div className='border-solid border-2 border-black rounded-lg bg-gray-300 m-8 md:mx-44 md:my-20 p-20'>
+        <p className='font-bold text-xl m-2'>Enter Email</p>
+        <input onBlur={handleEmailBlur} className='rounded p-2 w-full' type="email" placeholder='Type your email' required/>
+        <p className='font-bold text-xl m-2'>Enter Password</p>
+        <input onBlur={handlePasswordBlur} className='rounded p-2 w-full' type="password" placeholder='Type your password' required/>
+        <button className='bg-blue-600 hover:text-white m-4 px-6 py-2 rounded block mx-auto'>Log In</button>
+      </div>
+    </form>
   );
 }
 
